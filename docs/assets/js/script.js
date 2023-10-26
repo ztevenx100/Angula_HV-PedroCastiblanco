38c0ecd87c4dbe13aa8e7dbfab603851c03a408f
Steven Castiblanco <ztevenx100@gmail.com>
2023-10-26 10:38:22 -0500

    Build: delete folder Docs, change image path

--- docs/assets/js/script.js
+++
@@ -1,8 +0,0 @@
-// const nav = document.querySelector(".nav")
-//     , navList = nav.querySelectorAll("li")
-//     , totalNavList = navList.length;
-
-//     for (let i = 0; i < totalNavList; i++) {
-//         console.log(navList[i]);
-        
-//     }
--- src/app/component/UI/style-swicher/style-swicher.component.ts
+++ src/app/component/UI/style-swicher/style-swicher.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-style-swicher',
@@ -9,7 +9,56 @@ export class StyleSwicherComponent {
   scriptElementSwicher: HTMLScriptElement;
   constructor(){
     this.scriptElementSwicher = document.createElement("script");
-    this.scriptElementSwicher.src = "./assets/js/style-switcher.js";
-    document.body.appendChild(this.scriptElementSwicher);
+    // this.scriptElementSwicher.src = "./assets/js/style-switcher.js";
+    // document.body.appendChild(this.scriptElementSwicher);
   }
+
+  ngOnInit(){
+    // Variables
+    const styleSwitcherToggler: HTMLElement | null = document.querySelector(".style-switcher-toggler");
+    const styleSwitcher: HTMLElement | null = document.querySelector(".style-switcher");
+
+    // Open
+    styleSwitcherToggler?.addEventListener("click", () => {
+        styleSwitcher?.classList.toggle("open");
+    });
+
+    window.addEventListener("scroll", () => {
+        if (styleSwitcher?.classList.contains("open")) {
+            styleSwitcher.classList.remove("open");
+        }
+    });
+
+    // Theme light and dark
+    const dayNight: HTMLElement | null = document.querySelector(".day-night");
+
+    dayNight?.addEventListener("click", () => {
+        dayNight?.querySelector("i")!.classList.toggle("fa-sun");
+        dayNight?.querySelector("i")!.classList.toggle("fa-moon");
+        document.body.classList.toggle("dark");
+    });
+
+    window.addEventListener("load", () => {
+        if (document.body.classList.contains("dark")) {
+            dayNight?.querySelector("i")?.classList.add("fa-sun");
+        } else {
+            dayNight?.querySelector("i")?.classList.add("fa-moon");
+        }
+    });
+  }
+
+  setActiveStyle(color: string): void {
+      // Color
+      const alternateStyle: NodeListOf<HTMLElement> = document.querySelectorAll(".alternate-style");
+        alternateStyle.forEach((style) => {
+            if (color === style.getAttribute("title")) {
+                style.removeAttribute("disabled");
+            } else {
+                style.setAttribute("disabled", "true");
+            }
+        });
+    }
+
+
+
 }
