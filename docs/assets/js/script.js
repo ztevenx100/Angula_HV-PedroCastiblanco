49cf7db9e6b5faddd429e778020b92b02f72d715
Steven Castiblanco <ztevenx100@gmail.com>
2023-10-25 21:19:57 -0500

    Build: initial deploy project 23-10-25 - [Style] change locate import fonts

---
+++ docs/assets/js/script.js
@@ -0,0 +1,8 @@
+// const nav = document.querySelector(".nav")
+//     , navList = nav.querySelectorAll("li")
+//     , totalNavList = navList.length;
+
+//     for (let i = 0; i < totalNavList; i++) {
+//         console.log(navList[i]);
+        
+//     }
