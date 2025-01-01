40d730108a19fd43f8af67c23ba6f339cf82cada
Steven Castiblanco <50757337+ztevenx100@users.noreply.github.com>
2024-12-31 21:13:32 -0500

    TASK(about): Update performance counts

--- src/app/component/pages/about/about.component.ts
+++ src/app/component/pages/about/about.component.ts
@@ -29,8 +29,8 @@ export class AboutComponent implements OnInit{
 
   ngOnInit(): void {
     this.counterYears.actualizarCount(5, 600);
-    this.counterSolutions.actualizarCount(565,10);
-    this.counterRequest.actualizarCount(710,8);
+    this.counterSolutions.actualizarCount(580,10);
+    this.counterRequest.actualizarCount(780,8);
   }
 
 }
